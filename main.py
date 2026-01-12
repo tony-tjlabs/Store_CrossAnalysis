ab039245e9626b5704dac86f43067fbf193fc98b
Tony_mac <Tony_mac@Tony-mac.local>
2026-01-12 13:19:19 +0900

    Add password protection

--- main.py
+++ main.py
@@ -14,6 +14,35 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+# Password protection
+def check_password():
+    """Returns True if the user has entered the correct password."""
+    
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+    
+    if "password_correct" not in st.session_state:
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Incorrect password")
+        return False
+    else:
+        return True
+
+if not check_password():
+    st.stop()
+
 # Set Plotly default template for consistent black text
 pio.templates.default = "plotly_white"
 
